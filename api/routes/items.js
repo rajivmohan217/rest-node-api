a89ecddda0018978536d2ccf849a2e118662c276
rmohanra <rmohanra@RMOHANRA-LAP.oradev.oraclecorp.com>
2018-09-19 01:08:44 +0530

    services built

--- api/routes/items.js
+++ api/routes/items.js
@@ -2,15 +2,76 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+	host : process.env.DB_HOST,
+	user : process.env.DB_USER,
+	database : process.env.DB_NAME
+});
+
+connection.connect((err) => {
+	if (err) {
+		console.log(err);		
+	} else {
+		console.log('Connection Successfull!');
+		
+	}
+});
+
 router.get('/',(req, res, next) => {
-	res.status(200).json({
-		message : 'Handling GET Request!'
-	});
+	let sql = 'SELECT * FROM products_b' ;
+	connection.query(sql, (err, rows, fields) => {
+		if (err) {
+			res.status(500).json({
+				message : err.sqlMessage
+			});				
+		} else {
+			res.status(200);
+			res.send(rows);
+		}
+	});	
+});
+
+
+router.get('/:itemname',(req, res, next) => {
+	let sql = 'SELECT * FROM products_b where itemnumber ?';
+	connection.query(sql, req.param.itemname, (err, rows, fields) => {
+		if (err) {
+			res.status(500).json({
+				message: err.sqlMessage});			
+		} else {
+			res.status(200).json({
+				message : 'Handling GET Request!',
+				itemname : rows.itemnumber
+			});
+		}
+	});	
 });
 
 router.post('/',(req, res, next) => {
-	res.status(200).json({
-		message : 'Handling Post Request!'
+	let sql = 'INSERT INTO products_b SET ?';
+	var sqlValues = {
+		"itemnumber" : req.body.itemname,
+		"itemdescription" : req.body.description, 
+		"status" : req.body.status,
+		"price" : req.body.price
+	};
+	connection.query(sql, sqlValues, (err, rows, fields) => {
+		if (err) {
+			console.log(err);
+			res.status(500).json({
+				message : err.sqlMessage
+			});			
+		} else {
+			console.log('Record inserted');	
+			res.status(200).json({
+				message : 'Item is created!!',
+				itemnumber : req.body.itemname,
+				itemdescription : req.body.description, 
+				status : req.body.status,
+				price : req.body.price		
+			});
+	}
+	
 	});
 });
 
