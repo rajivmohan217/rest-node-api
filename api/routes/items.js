92b5e885629f64828f43a7dbc984f3fe92bf3099
rmohanra <rmohanra@RMOHANRA-LAP.oradev.oraclecorp.com>
2018-09-19 21:19:28 +0530

    changing rows to results

--- api/routes/items.js
+++ api/routes/items.js
@@ -3,104 +3,105 @@ const router = express.Router();
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-	host : process.env.DB_HOST,
-	user : process.env.DB_USER,
-	database : process.env.DB_NAME
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
 	if (err) {
-		console.log(err);		
+		console.log(err);
 	} else {
-		console.log('Connection Successfull!');		
+		console.log('Connection Successfull!');
 	}
 });
 
-router.get('/',(req, res, next) => {
-	let getALLItems = 'SELECT * FROM products_b' ;
-	connection.query(getALLItems, (err, rows, fields) => {
+router.get('/all', (req, res, next) => {
+	let getALLItems = 'SELECT * FROM products_b';
+	connection.query(getALLItems, (err, results, fields) => {
 		if (err) {
 			res.status(500).json({
-				Error : err.sqlMessage
-			});				
+				Error: err.sqlMessage
+			});
 		} else {
 			res.status(200);
-			res.send(rows);
+			res.send(results);
 		}
-	});	
+	});
 });
 
-router.get('/:itemname',(req, res, next) => {
+router.get('/:itemname', (req, res, next) => {
 	let singleItemQuery = 'SELECT * FROM PRODUCTS_B WHERE ITEMNUMBER=?';
-	connection.query(singleItemQuery, req.params.itemname, (err, rows, fields) => {
+	connection.query(singleItemQuery, req.params.itemname, (err, results, fields) => {
 		if (err) {
 			res.status(500).json({
-				message: err.sqlMessage});			
+				message: err.sqlMessage
+			});
 		} else {
 			res.status(200);
-			res.send(rows);
+			res.send(results);
 		}
-	});	
+	});
 });
 
-router.post('/',(req, res, next) => {
+router.post('/', (req, res, next) => {
 	let insertItems = 'INSERT INTO products_b SET ?';
 	var insertItemsValues = {
-		"itemnumber" : req.body.itemname,
-		"itemdescription" : req.body.description, 
-		"status" : req.body.status,
-		"price" : req.body.price
+		"itemnumber": req.body.itemname,
+		"itemdescription": req.body.description,
+		"status": req.body.status,
+		"price": req.body.price
 	};
-	connection.query(insertItems, insertItemsValues, (err, rows, fields) => {
-		if (err.code === 'ER_DUP_ENTRY') {			
+	connection.query(insertItems, insertItemsValues, (err, results, fields) => {
+		if (err.code === 'ER_DUP_ENTRY') {
 			res.status(500).json({
-				Error : `\'${req.body.itemname}\'  already exists`
-			});			
-		} else if(err) {
-			res.status(500).json({
-				Error : err.sqlMessage});
-		}
-		else {			
-			res.status(200).json({
-				message : 'Item is created!!',
-				itemnumber : req.body.itemname,
-				itemdescription : req.body.description, 
-				status : req.body.status,
-				price : req.body.price		
+				Error: `\'${req.body.itemname}\'  already exists`
 			});
-		}	
-	});
-});
-
-router.patch('/',(req, res, next) => {
-	let updateQuery  = 'UPDATE PRODUCTS_B SET itemdescription =?, price= ?, status= ? WHERE itemnumber = ? ';	
-	connection.query(updateQuery,[req.body.description, req.body.price, req.body.status, req.body.itemname],(err, results, fileds) => {
-		if (err) {
+		} else if (err) {
 			res.status(500).json({
-				Error : err.sqlMessage
+				Error: err.sqlMessage
 			});
-		} else{
+		} else {
 			res.status(200).json({
-				message : 'Item is updated!!',
-				itemnumber : req.body.itemname,
-				itemdescription : req.body.description, 
-				status : req.body.status,
-				price : req.body.price		
+				message: 'Item is created!!',
+				itemnumber: req.body.itemname,
+				itemdescription: req.body.description,
+				status: req.body.status,
+				price: req.body.price
 			});
 		}
 	});
 });
 
-router.delete('/',(req, res, next) => {
-	let deleteQuery  = 'DELETE FROM PRODUCTS_B WHERE itemnumber = ? ';	
-	connection.query(deleteQuery,req.body.itemname,(err, results, fileds) => {
+router.patch('/', (req, res, next) => {
+	let updateQuery = 'UPDATE PRODUCTS_B SET itemdescription =?, price= ?, status= ? WHERE itemnumber = ? ';
+	connection.query(updateQuery, [req.body.description, req.body.price, req.body.status, req.body.itemname], (err, results, fileds) => {
 		if (err) {
 			res.status(500).json({
-				Error : err.sqlMessage
+				Error: err.sqlMessage
 			});
-		} else{
+		} else {
 			res.status(200).json({
-				message : `\'${req.body.itemname}\' is deleted!!`				
+				message: 'Item is updated!!',
+				itemnumber: req.body.itemname,
+				itemdescription: req.body.description,
+				status: req.body.status,
+				price: req.body.price
+			});
+		}
+	});
+});
+
+router.delete('/', (req, res, next) => {
+	let deleteQuery = 'DELETE FROM PRODUCTS_B WHERE itemnumber = ? ';
+	connection.query(deleteQuery, req.body.itemname, (err, results, fileds) => {
+		if (err) {
+			res.status(500).json({
+				Error: err.sqlMessage
+			});
+		} else {
+			res.status(200).json({
+				message: `\'${req.body.itemname}\' is deleted!!`
 			});
 		}
 	});;
--- app.js
+++ app.js
@@ -5,18 +5,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(bodyParser.json());
 
-app.use('/items',itemRoutes);
+app.use('/items', itemRoutes);
 
-app.use((req , res , next) => {
+app.use((req, res, next) => {
 	const error = new Error('Not Found');
 	error.status = 404;
 	next(error);
 });
 
-app.use((error, req, res , next) => {
+app.use((error, req, res, next) => {
 	res.status(404).json({
 		error: {
 			message: error.message
